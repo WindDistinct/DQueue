9cd90c557b211263657c7eda58fdb3169bd3d322
Nicolas Acevedo <ao.nicolasr@gmail.com>
2026-08-24 14:30:53 -0500

    feat: implement queue logic

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"time"
 )
@@ -10,6 +11,10 @@ type Job struct {
 	Name      string
 	Payload   string
 	CreatedAt time.Time
+}
+
+type Queue struct {
+	jobs []*Job
 }
 
 var nextID = 1
@@ -27,17 +32,37 @@ func NewJob(name string, payload string) *Job {
 	return newJob
 }
 
+func (q *Queue) Enqueue(job *Job) {
+	q.jobs = append(q.jobs, job)
+}
+
+func (q *Queue) Dequeue() (job *Job, err error) {
+	if len(q.jobs) == 0 {
+		err = errors.New("La colas está vacía")
+		return nil, err
+	} else {
+		job = q.jobs[0]
+		q.jobs = q.jobs[1:]
+		return job, nil
+	}
+}
+
 func main() {
 	fmt.Println("DQueue starting...")
 
-	var createdJobs []*Job
+	queue := &Queue{}
 
-	createdJobs = append(createdJobs, NewJob("test 1", "payload 1"))
-	createdJobs = append(createdJobs, NewJob("test 2", "payload 2"))
-	createdJobs = append(createdJobs, NewJob("test 3", "payload 3"))
+	queue.Enqueue(NewJob("test 1", "payload 1"))
+	queue.Enqueue(NewJob("test 2", "payload 2"))
+	queue.Enqueue(NewJob("test 3", "payload 3"))
 
-	for i, job := range createdJobs {
-		fmt.Printf("Job Nro.%d queued: %s, %s\n", i, job.Name, job.Payload)
+	for i := 0; i < 4; i++ {
+		job, err := queue.Dequeue()
+		if err != nil {
+			fmt.Printf("%d: No hay más Jobs en la cola\n", i+1)
+		} else {
+			fmt.Printf("Se retiro el Job %d de la cola\n", job.ID)
+		}
 	}
 
 }
